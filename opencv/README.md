032979a7b8168624e0ca8b2d277b68a8b5a3da6b
kejin <liungkejin@gmail.com>
2024-07-20 18:50:34 +0800

    fix opencv/build.sh

---
+++ opencv/README.md
@@ -0,0 +1,8 @@
+# Usage
+
+```shell
+./build.sh -h
+
+./build.sh --os=android --abi=armeabi-v7a
+./build.sh --os=android --abi=arm64-v8a
+```
--- opencv/build.sh
+++ opencv/build.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+### Usage
+### ./build.sh --os=android --abi=armeabi-v7a
+### ./build.sh --os=android --abi=arm64-v8a
+
 SCRIPT_DIR=$(cd `dirname $0`; pwd)
 cd $SCRIPT_DIR && source ../platform_profile 
 
-name="opencv-4.9.0"
-repourl="git@github.com:opencv/opencv.git"
+# 处理源代码目录的相对路径
+ALL_ARGS=()
+USER_SOURCE_DIR=""
+for arg in "$@"; do
+    case "$arg" in
+        --source=*)
+            # 获取绝对路径
+            rel_path="${arg#--source=}"
+            USER_SOURCE_DIR="$(cd $rel_path && pwd)"
+            ALL_ARGS+=("--source=$USER_SOURCE_DIR")
+            ;;
+        *)
+            ALL_ARGS+=("$arg")
+            ;;
+    esac
+done
 
-if ! [ -d $name ]; then
-    echo "source code not found, clone from $repourl"
-    git clone --branch 4.9.0 $repourl $name
+if [[ -z "$USER_SOURCE_DIR" ]]; then
+    name="opencv-4.9.0"
+    repourl="git@github.com:opencv/opencv.git"
+
+    if ! [ -d $name ]; then
+        echo "source code not found, clone from $repourl"
+        git clone --branch 4.9.0 $repourl $name
+    fi
+
+    [[ -d $name ]] || exit -1
+
+    # 如果没有指定源代码目录，则使用默认目录
+    USER_SOURCE_DIR="$SCRIPT_DIR/$name"
+    ALL_ARGS+=("--source=$USER_SOURCE_DIR")
 fi
-cd $name || exit -1
 
 # 如果需要编译动态库
 # 修改 DBUILD_SHARED_LIBS=ON
@@ -23,4 +51,4 @@ native_build -DBUILD_SHARED_LIBS=OFF \
         -D BUILD_ANDROID_EXAMPLES=OFF \
         -D BUILD_DOCS=OFF \
         -D BUILD_PERF_TESTS=OFF \
-        -D BUILD_TESTS=OFF $@
+        -D BUILD_TESTS=OFF "${ALL_ARGS[@]}"
